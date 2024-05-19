09e126b8dbbb792733b6985f17c6fb4de5f05a8d
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-19 21:31:18 +0900

    포딩 메뉴얼 v2

--- in-project/script/build-docker-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Building Docker image..."
-REPOSITORY_NAME= "$1" #"wypl-web-dev"
-TAG="latest"
-docker build -t "$REPOSITORY_NAME":"$TAG" .
-
-# date tag 
-# DATE_TAG=$(date +%y%m%d%H%M)
-# docker build -t "$REPOSITORY_NAME":"$DATE_TAG" .
