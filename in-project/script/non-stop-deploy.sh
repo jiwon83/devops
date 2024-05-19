9d6e4fe5f241b267cc36b5d19e42c214165ccbc8
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-19 14:43:30 +0900

    포팅포워드 문서 정리 v1

--- in-project/script/non-stop-deploy.sh
+++ in-project/script/non-stop-deploy.sh
@@ -4,33 +4,35 @@ IMAGE_NAME=wypl-web
 TAG=latest
 DEPLOY_PORT1=58324
 DEPLOY_PORT2=58325
-DEPLOY_PROFILE1=prod1
-DEPLOY_PROFILE2=prod2
+DEPLOY_PROFILE1=blue
+DEPLOY_PROFILE2=green
+SERVER_IP_ADDR="43.203.229.26"
+IDLE_PROFILE=""
+IDLE_PORT=""
+HOST_VOLUME_PATH=/home/ubuntu/wypl-deploy/logs
+DOCKER_VOLUME_PATH=/logs
 
 ######### Check IDLE PROFILE & PORT ############
 CURR_PROFILE=$(curl -s --connect-timeout 10 https://api.wypl.site/profile  | tr -d '\n' | tr -d '\r')
 
-echo "profile > $CURR_PROFILE"
-#[ "$CURR_PROFILE" == *"$DEPLOY_PROFILE1"* ]
+echo "현재 PROFILE > $CURR_PROFILE"
 
-if [[ "$CURR_PROFILE" == *"prod1"* ]]; then
-    echo "Debug: Current profile is DEPLOY_PROFILE1"
+if [[ "$CURR_PROFILE" == *"$DEPLOY_PROFILE1"* ]]; then
     IDLE_PROFILE="$DEPLOY_PROFILE2"
     IDLE_PORT="$DEPLOY_PORT2"
-elif [ "$CURR_PROFILE" == "$DEPLOY_PROFILE2" ]; then
-    echo "Debug: Current profile is DEPLOY_PROFILE2"
+elif [[ "$CURR_PROFILE" == *"$DEPLOY_PROFILE2"* ]]; then
     IDLE_PROFILE="$DEPLOY_PROFILE1"
     IDLE_PORT="$DEPLOY_PORT1"
 else
     echo "Debug: No matching Profile found. Profile: $CURR_PROFILE"
-    echo "> Assigning $DEPLOY_PROFILE1."
+    echo "> $DEPLOY_PROFILE1 으로 설정합니다."
     IDLE_PROFILE="$DEPLOY_PROFILE1"
     IDLE_PORT="$DEPLOY_PORT1"
 fi
 echo "IDLE_PROFILE > $IDLE_PROFILE IDLE_PROT > $IDLE_PORT "
 
 ######### Build Docker Image ############
-echo "Building Docker image..."s
+echo "######### Building Docker image ############"
 IDLE_APPLICATION_NAME="$IDLE_PROFILE-$IMAGE_NAME"
 ACTIVE_APPLICATION_NAME="$CURR_PROFILE-$IMAGE_NAME"
 
@@ -39,6 +41,7 @@ echo "ACTIVE_APPLICATION_IMAGE_NAME > $ACTIVE_APPLICATION_NAME"
 docker build -t "$IDLE_APPLICATION_NAME":"$TAG" .
 
 ######### IDLE Deploy ############
+echo "######### Deploy ############"
 echo "> $IDLE_PROFILE 배포"
 
 CONTAINER_IDS=$(docker ps -aqf "name=$IDLE_APPLICATION_NAME")
@@ -51,13 +54,12 @@ if [ -n "$CONTAINER_IDS" ]; then
         docker rm $container_id
     done
 fi
-docker run -d --name $IDLE_APPLICATION_NAME -e PROFILE=$IDLE_PROFILE -p $IDLE_PORT:8080 $IDLE_APPLICATION_NAME:$TAG
+docker run -d --name $IDLE_APPLICATION_NAME -e PROFILE=$IDLE_PROFILE -p $IDLE_PORT:8080 -v $HOST_VOLUME_PATH:$DOCKER_VOLUME_PATH $IDLE_APPLICATION_NAME:$TAG
 
 ######### HEALTH CHECK ############
-
-echo "> $IDLE_PROFILE 10초 후 Health check 시작"
-echo "> curl -s http://api.wypl.site/actuator/health"
-sleep 10
+echo "######### HEALTH CHECK ############"
+echo "> $IDLE_PROFILE 3초 후 Health check 시작"
+sleep 3
 
 URL="https://api.wypl.site/actuator/health"
 MAX_TRIES=10
@@ -73,7 +75,7 @@ while [ $tries -lt $MAX_TRIES ]; do
         tries=$((tries+1))
         if [ $tries -lt $MAX_TRIES ]; then
             echo "Retrying in 10 seconds..."
-            sleep 10
+            sleep 3
         else
             echo "Failed: Server Is Down..."
             exit 1
@@ -82,14 +84,14 @@ while [ $tries -lt $MAX_TRIES ]; do
 done
 
 ######### NGINX RELOAD ############
+echo "######### NGINX RELOAD ############"
 echo "> 전환할 Port: $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$deploy_service_url http://43.203.229.26:${IDLE_PORT};" |sudo tee /etc/nginx/sites-available/deploy-service-url.inc
-
-PROXY_PROFILE=$(curl -s http://api.wypl.site/profile)
+echo "set \$deploy_service_url http://${SERVER_IP_ADDR}:${IDLE_PORT};" |sudo tee /etc/nginx/sites-available/deploy-service-url.inc
+sudo service nginx reload
+sleep 3
+PROXY_PROFILE=$(curl -s --connect-timeout 10 https://api.wypl.site/profile)
 echo "> Nginx Current Proxy Profile: $PROXY_PROFILE"
 
-echo "> Nginx Reload"
 
-sudo service nginx reload
-
+######### NGINX RELOAD ############
+# 최종 수정일 : 24-05-15
